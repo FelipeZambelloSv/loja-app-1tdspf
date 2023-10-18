427c365a55aa6af57d0fb02b80e6744d9fd54b51
FelipeZambelloSv <rm99843@fiap.com.br>
2023-10-18 11:08:40 -0300

    Rotas Dinamicas com parametros

---
+++ src/app/Produtos/mosfet/[slug]/page.jsx
@@ -0,0 +1,8 @@
+export default function ProdutoSlug({params}) {
+    return (
+        <div>
+            <h1>Produto SLUG</h1>
+            <p>Slug: {params.slug}</p>
+        </div>
+    )
+}
---
+++ src/app/Produtos/tocas/[id]/page.jsx
@@ -0,0 +1,8 @@
+export default function ProdutoId({params}) {
+    return(
+        <div>
+            <h1>Produto ID</h1>
+            <p>Slug: {params.id}</p>
+        </div>
+    )
+}
